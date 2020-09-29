5896374666b214c1b0193418284a22966f3344a6
Orhan Yarar <orhan@hotmail.ca>
2020-09-28 21:36:01 -0400

    init

---
+++ build/precache-manifest.63d8f6384eaae04a194d1e4a00707f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd152062da1e1a8556278870b65dd009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86d099942e02e6de053b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "929940630fed22efd26d",
+    "url": "/static/js/2.283ecc64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.283ecc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d099942e02e6de053b",
+    "url": "/static/js/main.f8c3d25a.chunk.js"
+  },
+  {
+    "revision": "0215e65f356a75c06fb7",
+    "url": "/static/js/runtime-main.20564b72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
